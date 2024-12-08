b381802d70c76be445cf1697a5a359d86617e429
atul-ramkrishnan <atul.ramkrishnan97@gmail.com>
2024-12-07 23:44:16 -0500

    Respond with 200

--- app/main.py
+++ app/main.py
@@ -5,11 +5,9 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    connection, address = server_socket.accept() # wait for client
+    connection.sendall('HTTP/1.1 200 OK\r\n\r\n'.encode())
 
 if __name__ == "__main__":
     main()
