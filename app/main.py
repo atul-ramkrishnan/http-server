582eb189225943146ef803f4e198b8d57aa8e4f8
atul-ramkrishnan <atul.ramkrishnan97@gmail.com>
2024-12-11 19:47:58 -0500

    Refactor code

--- app/main.py
+++ app/main.py
@@ -2,57 +2,103 @@ import socket
 import threading
 import argparse
 import os
+from .HttpRequest import HttpRequest
+from .HttpResponse import HttpResponse
+
+
+def handle_get_request(request, args):
+    response_headers = {}
+    if request.headers.get("Accept-Encoding", "") == "gzip":
+        response_headers["Content-Encoding"] = "gzip"
+    response = None
+    if request.path == "/":
+        response_headers["Content-Type"] = "text/html"
+        response = HttpResponse(status_code=200,
+                                status_message="OK",
+                                headers=response_headers)
+    elif request.path.startswith("/echo"):
+        responseBody = request.path.split("/echo/")[1]
+        response_headers["Content-Type"] = "text/plain"
+        response_headers["Content-Length"] = str(len(responseBody))
+        response = HttpResponse(status_code=200,
+                                status_message="OK",
+                                body=responseBody,
+                                headers=response_headers)
+    elif request.path.startswith("/files"):
+        filePath = args.directory + request.path.split("/files")[1]
+        response_headers["Content-Type"] = "application/octet-stream"
+        response_headers["Content-Length"] = str(os.path.getsize(filePath))
+        try:
+            with open(filePath, "r") as file:
+                responseBody = file.read()
+                response = HttpResponse(status_code=200,
+                                        status_message="OK",
+                                        body=responseBody,
+                                        headers=response_headers)
+        except FileNotFoundError:
+            response = HttpResponse(status_code=404,
+                                    status_message="Not Found")
+    elif request.path == "/user-agent":
+        userAgent = request.headers.get("User-Agent", "")
+        response_headers["Content-Type"] = "text/plain"
+        response_headers["Content-Length"] = str(len(userAgent))
+        response = HttpResponse(status_code=200,
+                                status_message="OK",
+                                body=userAgent,
+                                headers=response_headers)
+    else:
+        response = HttpResponse(status_code=404,
+                                status_message="Not Found")
+    return response
+
+
+def handle_post_request(request, args):
+    response = None
+    if request.path.startswith("/files"):
+        filePath = args.directory + request.path.split("/files")[1]
+        with open(filePath, "w") as file:
+            file.write(request.body)
+        response = HttpResponse(status_code=201,
+                                status_message="Created")
+    else:
+        response = HttpResponse(status_code=404,
+                                status_message="Not Found")
+    return response
+
+
+def handle_method_not_allowed(connection, request, args):
+    response = HttpResponse(status_code=405, status_message="Method Not Allowed")
+    return response
+
+
+def handle_request(connection, request, args):
+    response = None
+    if request.method == 'GET':
+        response = handle_get_request(request, args)
+    elif request.method == 'POST':
+        response = handle_post_request(request, args)
+    else:
+        response = handle_method_not_allowed(request, args)
+    
+    connection.sendall(str(response).encode())
+    connection.close()
+
 
 def handle_client(connection, args):
-    status_line = ""
-    headers = ""
-    response_body = ""
     data = connection.recv(1024).decode() # receive data
-    method = data.split()[0] # get method from request
-    path = data.split()[1] # get path from request
-    if method == "GET":
-        if path == "/":
-            status_line = 'HTTP/1.1 200 OK\r\n'
-            headers = "Content-Type: text/html\r\n"
-        elif path.startswith("/echo"):
-            status_line = 'HTTP/1.1 200 OK\r\n'
-            response_body = path.split("/echo/")[1]
-            headers = f"Content-Type: text/plain\r\nContent-Length: {len(response_body)}\r\n"
-        elif path.startswith("/file"):
-            file_path = args.directory + path.split("/files")[1]
-            try:
-                with open(file_path, "r") as file:
-                    status_line = 'HTTP/1.1 200 OK\r\n'
-                    response_body = file.read()
-                    headers = f"Content-Type: application/octet-stream\r\nContent-Length: {os.path.getsize(file_path)}\r\n"
-            except FileNotFoundError:
-                status_line = 'HTTP/1.1 404 Not Found\r\n'
-        elif path == "/user-agent":
-            status_line = 'HTTP/1.1 200 OK\r\n'
-            response_body = data.split("User-Agent: ")[1].split("\r\n")[0]
-            headers = f"Content-Type: text/plain\r\nContent-Length: {len(response_body)}\r\n"            
-        else:
-            status_line = 'HTTP/1.1 404 Not Found\r\n'
-        
-    elif method == 'POST':
-        if path.startswith("/files"):
-            file_path = args.directory + path.split("/files")[1]
-            header_section, body = data.split('\r\n\r\n', 1)
-            with open(file_path, 'w') as file:
-                file.write(body)
-            status_line = 'HTTP/1.1 201 Created\r\n'
-    else:
-        status_line = 'HTTP/1.1 405 Method Not Allowed\r\n'
+    request = HttpRequest(data)
+    print(request)
 
-    response = status_line + headers + "\r\n" + response_body
-    connection.sendall(response.encode())
-    connection.close()
+    return handle_request(connection, request, args)
+
+
 def main():
     print("Server is running on localhost:4221")
     parser = argparse.ArgumentParser()
     parser.add_argument('--directory', type=str, default=".")
     args = parser.parse_args()
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+
     try:
         while True:
             connection, address = server_socket.accept() # wait for client
@@ -61,5 +107,7 @@ def main():
     except KeyboardInterrupt:
         print("Server is shutting down")
         server_socket.close()
+
+
 if __name__ == "__main__":
     main()
